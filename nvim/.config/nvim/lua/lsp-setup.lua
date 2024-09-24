c4a62343cdc5aedf562d29a90416cfc55cc53be7
BobTheHobo <thienvietngomai@gmail.com>
2024-09-24 05:25:19 -0400

    Fix whichkey and lsp problem, edit oil

--- nvim/.config/nvim/lua/lsp-setup.lua
+++ nvim/.config/nvim/lua/lsp-setup.lua
@@ -44,15 +44,16 @@ local on_attach = function(_, bufnr)
 end
 
 -- document existing key chains
-require('which-key').register {
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-  ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-  ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
-  ['<leader>h'] = { name = 'More git', _ = 'which_key_ignore' },
-  ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-  ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-  ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-}
+local wk = require("which-key")
+wk.add({
+  { '<leader>c', desc = '[C]ode',      hidden = true },
+  { '<leader>d', desc = '[D]ocument',  hidden = true },
+  { '<leader>g', desc = '[G]it',       hidden = true },
+  { '<leader>h', desc = 'More git',    hidden = true },
+  { '<leader>r', desc = '[R]ename',    hidden = true },
+  { '<leader>s', desc = '[S]earch',    hidden = true },
+  { '<leader>w', desc = '[W]orkspace', hidden = true },
+})
 
 -- mason-lspconfig requires that these setup functions are called in this order
 -- before setting up the servers.
@@ -72,7 +73,7 @@ local servers = {
   -- gopls = {},
   pyright = {},
   rust_analyzer = {},
-  tsserver = {},
+  ts_ls = {}, --[[ typescript ]]
   html = { filetypes = { 'html', 'twig', 'hbs' } },
   jdtls = {},
   asm_lsp = {
