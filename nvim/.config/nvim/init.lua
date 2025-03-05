2f2b7212635e233d4541621d33c47cc9fe563112
BobTheHobo <thienvietngomai@gmail.com>
2025-03-05 05:47:36 -0500

    Add omnisharp, gdscript

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -32,6 +32,7 @@ require('lazy').setup({
       -- Automatically install LSPs to stdpath for neovim
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
+      "Hoffs/omnisharp-extended-lsp.nvim", -- For omnisharp
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
