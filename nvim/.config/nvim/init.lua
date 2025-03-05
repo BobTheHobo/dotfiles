d81439293b343e38efae34f795458cb96173d4ae
Thienviet Ngomai <thienvietngomai@gmail.com>
2025-03-05 01:00:45 -0500

    Modify lualine, move to it's own plugin file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -133,21 +133,6 @@ require('lazy').setup({
   --   end
   -- },
 
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'onedark',
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-      },
-    },
-  },
-
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
