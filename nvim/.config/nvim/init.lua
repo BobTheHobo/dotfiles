bc761796888e7605aa1720593e797e699f2f387e
Thienviet Ngomai <thienvietngomai@gmail.com>
2025-03-05 04:29:50 -0500

    Add harpoon

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -180,14 +180,13 @@ require('lazy').setup({
     import = "plugins"
   },
 
-
-  -- LSP --
-  require 'lsp-setup'
-
 },
   -- Stop lazy from notifying every time a change is made
   { change_detection = { notify = false } }
 )
+
+-- LSP --
+require 'lsp-setup'
 
 -- Options --
 require 'options'
