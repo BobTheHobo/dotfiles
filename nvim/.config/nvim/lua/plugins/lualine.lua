6cf67b0de36110453d6556686f123c317b6e19c0
Thienviet Ngomai <thienvietngomai@gmail.com>
2025-03-05 01:11:48 -0500

    Revert some lualine changes

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,35 @@
+-- Set lualine as statusline
+return {
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'onedark',
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = {'mode'},
+      lualine_b = {'branch', 'diff', 'diagnostics'},
+      lualine_c = {'filename'},
+      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {
+        { 'selectioncount', padding = { left = 0, right = 1 } },
+        { 'searchcount', maxcount = 999, timeout = 500, },
+        'location'
+      }
+    },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = {'filename'},
+      lualine_x = {'location'},
+      lualine_y = {},
+      lualine_z = {}
+    },
+    extensions = {"aerial", "oil"},
+  },
+}
